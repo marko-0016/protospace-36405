b0012b7b2e7c2a7201c8ffb78c423fbb744bae5d
marko-0016 <87635867+marko-0016@users.noreply.github.com>
2021-08-13 14:24:28 +0900

    エラー修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
-  before_action :move_to_index, except: [:index, :show, :search]
 
   # before_action :move_to_index, only: [:edit, :update]
   # ここにオプションを追記するauthenticate_user!をどのアクションで使うのか
@@ -26,7 +25,6 @@ def show
   @prototype = Prototype.find(params[:id])
   @comment = Comment.new
   @comments = @prototype.comments.includes(:prototype)
-  redirect_to root_path
 end
 
 def update
@@ -40,6 +38,9 @@ end
 
 def edit
   @prototype = Prototype.find(params[:id])
+  if current_user !=  @prototype.user
+    redirect_to root_path
+  end
 end
 
 def destroy
